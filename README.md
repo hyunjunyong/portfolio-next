59780657b9d4d8a70dd03e44c4b7c53408f3b52b
현준용 <hjy4649@cocen.com>
2023-11-12 14:24:42 +0900

    feat(ui): add nextui

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# portfolio-next
+# 포트폴리오 (next.js 14버전)
+
+## URL : https://portfolio-hyunjunyong.vercel.app/
+nuxt로만들어진 해당 포트폴리오를 next로도 제작중입니다.
+
+## URL : 
+
+## Next14로 제작되었으며 githubPages로 배포예정입니다..
---
+++ src/app/components/TheHeader.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import { Link } from '@nextui-org/react';
+
+export default function Home() {
+  return (
+    <header>
+      <Link color="success" href="/">
+        <span className="text-2xl font-bold xs:text-xl xs:font-semibold">
+          Hyun's Portfolio
+        </span>
+      </Link>
+
+      <ul className="flex gap-3 items-center justify-center">
+        <li>
+          <Link color="success" href="/" className="text-xl">
+            Home
+          </Link>
+        </li>
+
+        <li>
+          <Link color="success" href="/project/list" className="text-xl">
+            Project
+          </Link>
+        </li>
+        {/* <li>
+          <el-switch
+            v-model="isDark"
+            className="ml-2"
+            width="40"
+            style="
+              --el-switch-on-color: #333333;
+              --el-switch-off-color: #bebebe;
+            "
+            :active-action-icon="Moon"
+            :inactive-action-icon="Sunny"
+          />
+        </li> */}
+      </ul>
+    </header>
+  );
+}
