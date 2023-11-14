06ae7db3a3b368f6606255bf14e387f748924fe6
현준용 <hjy4649@cocen.com>
2023-11-14 14:13:44 +0900

    fix(type): subtitle component type fix

--- src/app/components/layout/subTitle.tsx
+++ src/app/components/layout/subTitle.tsx
@@ -1,13 +1,9 @@
 import React, { ReactNode } from 'react';
 
-interface ChildComponentProps {
-  children: ReactNode;
-}
-
-const subTitle: React.FC<ChildComponentProps> = ({ children }) => {
+const subTitle = (props: ReactNode) => {
   return (
     <div className="py-2">
-      <span className="text-base font-bold">❗️{children}</span>
+      <span className="text-base font-bold">❗️{props}</span>
       <slot name="content"></slot>
     </div>
   );
